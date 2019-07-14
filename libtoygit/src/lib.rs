84d4582447ebe5f023b9bca49fb22a356d5646d3
Benjamin Levy <blevy@protonmail.com>
2019-07-14 18:58:42 -0400

    Add config and description writing

--- libtoygit/src/lib.rs
+++ libtoygit/src/lib.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
-extern crate serde_ini;
+extern crate tini;
 
 use std::env;
 use std::ffi::OsString;
@@ -8,8 +8,19 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
+use tini::Ini;
+
+const DEFAULT_DESCRIPTION: &str =
+    "Unnamed repository; edit this file 'description' to name the repository.\n";
+
 lazy_static! {
     static ref GIT_DIR: OsString = env::var_os("GIT_DIR").unwrap_or_else(|| OsString::from(".git"));
+    static ref DEFAULT_CONFIG: Ini = Ini::new()
+        .section("core")
+        .item("repositoryformatversion", "0")
+        .item("filemode", "true")
+        .item("bare", "false")
+        .item("logallrefupdates", "true");
 }
 
 /// Create an empty Git repository or reinitialize an existing one.
@@ -29,8 +40,8 @@ pub fn init(dir: &Path) -> io::Result<()> {
     fs::create_dir(refs_dir.join("heads"))?;
     fs::create_dir(refs_dir.join("tags"))?;
 
-    fs::File::create(git_dir.join("config"))?; // TODO: write contents
-    fs::File::create(git_dir.join("description"))?; // TODO: write contents
+    DEFAULT_CONFIG.to_file(&git_dir.join("config"))?;
+    fs::write(git_dir.join("description"), DEFAULT_DESCRIPTION)?;
     fs::File::create(git_dir.join("HEAD"))?; // TODO: write contents
 
     Ok(())
