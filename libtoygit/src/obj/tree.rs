eb9392167447b6af8faded598d8b1c1d9704953d
Benjamin Levy <blevy@protonmail.com>
2019-07-30 15:32:38 -0400

    dev

--- libtoygit/src/obj/tree.rs
+++ libtoygit/src/obj/tree.rs
@@ -73,8 +73,17 @@ impl WalkTreeItem {
     /// This path is relative to the repository root.
     pub fn path(&self) -> PathBuf {
         let filename = self.node.filename.to_os_string();
-        // repo/parent_path/filename
-        self.repo.join(self.parent_path.join(filename))
+        let path = self.repo.join(self.parent_path.join(&filename));
+
+        debug!(
+            "WalkTreeItem path: ({}, {}, {}) -> {}",
+            self.repo.display(),
+            self.parent_path.display(),
+            filename.to_string_lossy(),
+            path.display()
+        );
+
+        path
     }
 
     /// Get a list of the node's children. If this node is not a
